1ffaf46f062e425e15112ea5795722aa6d000789
Raphael Freitas Novo <rfn_vip@hotmail.com>
2023-04-09 09:45:49 -0300

    opepage

--- script.js
+++ script.js
@@ -1,31 +1,49 @@
 (() => {
   $ = jQuery;
 
+  /* Menu */
+
+  $(document).on('click', 'footer a', function(){
+
+    const menu = $(this).data('menu');
+
+    $(this).parents('footer').find('a').removeClass('active');
+    $(this).addClass('active');
+
+    $(document).find('main.content').removeClass('active');
+    $(document).find(`main.content#${menu}`).addClass('active');
+
+    updateEggsBasket();
+    
+  });
+
   /* Eggs */
+
+
   function updateEggsBasket(){
     const eggs = $(document).find('.eggs');
+    const eggs_storage = window.localStorage.getItem('eggs');
+    let foundEggs;
+  
+    if(eggs_storage !== ''){
+      foundEggs = JSON.parse(eggs_storage) || [];
+    } else {
+      foundEggs = [];
+    }
+  
+    if(!Array.isArray(foundEggs)){
+      foundEggs = [];
+    }
 
     if( eggs !== 'undefined'){
+
+      eggs.html('');
 
       for(let i = 1; i <= 12; i++){
         eggs.append(`
                     <div class="egg disabled" key="${i}">
                       <img src="img/ovos/${i}.png" />
                     </div>`);
-      }
-
-      
-      const eggs_storage = window.localStorage.getItem('eggs');
-      let foundEggs;
-
-      if(eggs_storage !== ''){
-        foundEggs = JSON.parse(eggs_storage) || [];
-      } else {
-        foundEggs = [];
-      }
-
-      if(!Array.isArray(foundEggs)){
-        foundEggs = [];
       }
 
       let numFound = 0;
@@ -41,8 +59,6 @@
 
   }
 
-  updateEggsBasket();
-
 
   /* QR */
 
@@ -50,72 +66,79 @@
   
     function onScanSuccess(decodedText, decodedResult) {
 
+      if($('main.content#eggs').hasClass('active')){
 
-      if(decodedText.includes('egg---')){
-        const eggs_storage = window.localStorage.getItem('eggs');
-        let foundEggs;
+        if(decodedText.includes('egg---')){
 
-        if(eggs_storage !== ''){
-          foundEggs = JSON.parse(eggs_storage) || [];
-        } else {
-          foundEggs = [];
-        }
+          const eggNumber = decodedText.split('egg---');
 
-        if(!Array.isArray(foundEggs)){
-          foundEggs = [];
-        }
+          if(typeof (eggNumber[1]) !== 'undefined'){
+            const number = parseInt(eggNumber[1]);
 
-        const eggNumber = decodedText.split('egg---');
+            if(number > 0 && number <=12){
+              
+              const eggs_storage = window.localStorage.getItem('eggs');
+              let foundEggs;
+            
+              if(eggs_storage !== ''){
+                foundEggs = JSON.parse(eggs_storage) || [];
+              } else {
+                foundEggs = [];
+              }
+            
+              if(!Array.isArray(foundEggs)){
+                foundEggs = [];
+              }
 
-        if(typeof (eggNumber[1]) !== 'undefined'){
-          const number = parseInt(eggNumber[1]);
+              if(!foundEggs.includes(number)){
 
-          if(number > 0 && number <=12){
-            if(!foundEggs.includes(number)){
+                html5QrcodeScanner = false;
 
-              html5QrcodeScanner = false;
+                foundEggs.push(number);
 
-              foundEggs.push(number);
+                $('main.content#eggs').addClass('novo');
 
-              $('main.content').addClass('novo');
+                $('main.content#eggs .img_novo_ovo').html(`
+                  <h1>Novo Ovo encontrado!</h1>
+                  <img src="img/ovos/${number}.png" class="novo_ovo" />
+                `);
 
-              $('main.content').html(`
-                <h1>Novo Ovo encontrado!</h1>
-                <img src="img/ovos/${number}.png" class="novo_ovo" />
-              `);
+                $('main.content#eggs h1').text('Novo Ovo encontrado!');
 
-              window.localStorage.setItem('eggs', JSON.stringify(foundEggs));
+                window.localStorage.setItem('eggs', JSON.stringify(foundEggs));
 
-              setTimeout(() => {
-                window.location = 'eggs.html';
-              }, 3000);
+                setTimeout(() => {
+                  $(document).find('a[data-menu="eggs"]').trigger('click');
+                  $('main.content#eggs').removeClass('novo');
+                }, 3000);
 
-            } else if(!$('main.content').hasClass('novo')) {
-              $('main.content h1').text('Você já achou este ovo antes!');
-              setTimeout(() => {
-                $('main.content h1').text('');
-              }, 2000);
+              } else if(!$('main.content#eggs').hasClass('novo')) {
+                $('main.content#eggs h1').text('Você já achou este ovo antes!');
+                setTimeout(() => {
+                  $('main.content h1').text('');
+                }, 2000);
+              }
+            } else {
+              $('main.content#eggs h1').text('QR Code Inválido!');
+                setTimeout(() => {
+                  $('main.content#eggs h1').text('');
+                }, 2000);
             }
-          } else {
-            $('main.content h1').text('QR Code Inválido!');
-              setTimeout(() => {
-                $('main.content h1').text('');
-              }, 2000);
           }
+
         }
 
-      }
+        if(decodedText.includes('egg--reset')){
+          window.localStorage.setItem('eggs', []);
+          $('main.content#eggs h1').text('Ovos apagados!');
+          setTimeout(() => {
+            $('main.content#eggs h1').text('');
+          }, 2000);
+        }
 
-      if(decodedText.includes('egg--reset')){
-        window.localStorage.setItem('eggs', []);
-        $('main.content h1').text('Ovos apagados!');
-        setTimeout(() => {
-          $('main.content h1').text('');
-        }, 2000);
+        // handle the scanned code as you like, for example:
+        console.log(`Code matched = ${decodedText}`, decodedResult);
       }
-
-      // handle the scanned code as you like, for example:
-      console.log(`Code matched = ${decodedText}`, decodedResult);
     }
     
     function onScanFailure(error) {
